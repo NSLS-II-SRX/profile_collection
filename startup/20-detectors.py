321f48ade5e212386e485e0676522f055f6a4ebd
Thomas A Caswell <tcaswell@bnl.gov>
2016-02-25 13:51:37 -0500

    HACK: wait on trigger in F460 staging

--- startup/20-detectors.py
+++ startup/20-detectors.py
@@ -1,6 +1,8 @@
 from ophyd import ProsilicaDetector, EpicsSignal, Device
 from ophyd import Component as Cpt
 from ophyd.ophydobj import StatusBase
+from ophyd.status import wait
+
 import time as ttime
 
 hfm_cam = EpicsSignal('XF:05IDA-BI:1{FS:1-Cam:1}Acquire_RBV',
@@ -87,7 +89,7 @@ class CurrentPreamp(Device):
         # or just use pyepics directly if you need to
         super().stage()
         self.initi_trigger.put(1, wait=True)
-        self.trigger()
+        wait(self.trigger())
 
     def trigger(self):
         init_ts = self.ch0.timestamp
