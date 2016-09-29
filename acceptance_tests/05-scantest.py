3d14e4dd3cc006eeb53620ae73efa5adf7a2cb74
SRX Operator <xf05id1@xf05id1-ws2.cs.nsls2.local>
2016-09-29 16:32:48 -0400

    update scan acceptance test to not try to open shutter.

--- acceptance_tests/05-scantest.py
+++ acceptance_tests/05-scantest.py
@@ -9,4 +9,4 @@ from time import sleep as tsleep
 print('5. testing integrated scan functions:')
 
 print('testing xrf 2d mapping')
-hf2dxrf(xstart=26.70,xstepsize=0.005,xnumstep=3,ystart=15.28,ystepsize=.005,ynumstep=3,acqtime=.2,numrois=2, i0map_show=True, itmap_show=True)
+RE(hf2dxrf(xstart=26.70,xstepsize=0.005,xnumstep=3,ystart=15.28,ystepsize=.005,ynumstep=3,acqtime=.2,numrois=2, i0map_show=True, itmap_show=True,shutter=False))
