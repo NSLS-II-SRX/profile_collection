b96e5cb8e31661f402079da6b208e25125c40e02
SRX Operator <xf05id1@xf05id1-ws1.cs.nsls2.local>
2016-09-30 09:58:38 -0400

    removed old pseudo motors.

--- startup/30-pseudomotors.py
+++ startup/30-pseudomotors.py
@@ -6,9 +6,9 @@ import srxbpm
 import tempdev
 import srxm2
 
-wb=srxslit.nsls2slit(tb='XF:05IDA-OP:1{Slt:1-Ax:T}',bb='XF:05IDA-OP:1{Slt:1-Ax:B}',ib='XF:05IDA-OP:1{Slt:1-Ax:I}',ob='XF:05IDA-OP:1{Slt:1-Ax:O}')
-pb=srxslit.nsls2slit(ib='XF:05IDA-OP:1{Slt:2-Ax:I}',ob='XF:05IDA-OP:1{Slt:2-Ax:O}')
-ssa=srxslit.nsls2slit(tb='XF:05IDB-OP:1{Slt:SSA-Ax:T}', bb='XF:05IDB-OP:1{Slt:SSA-Ax:B}', ob='XF:05IDB-OP:1{Slt:SSA-Ax:O}',ib='XF:05IDB-OP:1{Slt:SSA-Ax:I}')
+#wb=srxslit.nsls2slit(tb='XF:05IDA-OP:1{Slt:1-Ax:T}',bb='XF:05IDA-OP:1{Slt:1-Ax:B}',ib='XF:05IDA-OP:1{Slt:1-Ax:I}',ob='XF:05IDA-OP:1{Slt:1-Ax:O}')
+#pb=srxslit.nsls2slit(ib='XF:05IDA-OP:1{Slt:2-Ax:I}',ob='XF:05IDA-OP:1{Slt:2-Ax:O}')
+#ssa=srxslit.nsls2slit(tb='XF:05IDB-OP:1{Slt:SSA-Ax:T}', bb='XF:05IDB-OP:1{Slt:SSA-Ax:B}', ob='XF:05IDB-OP:1{Slt:SSA-Ax:O}',ib='XF:05IDB-OP:1{Slt:SSA-Ax:I}')
 m2x=srxm2.mottwin(m1='XF:05IDD-OP:1{Mir:2-Ax:XU}Mtr',m2='XF:05IDD-OP:1{Mir:2-Ax:XD}Mtr')
 # bpm1=srxbpm.nsls2bpm(bpm='bpm1')
 # bpm2=srxbpm.nsls2bpm(bpm='bpm2')
