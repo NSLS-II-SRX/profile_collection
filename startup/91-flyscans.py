9ede39cdbbca6d912279a93c6d9774730aaa0bfa
SRX Operator <xf05id1@xf05id1-ws1.cs.nsls2.local>
2016-10-07 14:53:30 -0400

    WIP: flying the zebra and xpress3

--- startup/91-flyscans.py
+++ startup/91-flyscans.py
@@ -15,11 +15,17 @@
 #Zebra staged, data read and written to file, file registered
 #Options:  open and write to Xspress3 HDF5, write to HDF5 and link in 
 #   Xspress3 data, write separately in some format and register later
+#
+from filestore import filestore.commands as fs
+import uuid
 
 class SRXFlyer1Axis:
-
+    LARGE_FILE_DIRECTORY_READ_PATH = '/tmp/test_data'
+    LARGE_FILE_DIRECTORY_WRITE_PATH = '/tmp/test_data'
+    "This is the Zebra."
 #    def __init__(self, encoder, xspress3, motor, start, incr, dwell, Npts=1000):
-    def __init__(self, encoder, motor, start, incr, dwell, Npts=1000, delta):
+    def __init__(self, encoder, motor, start, incr, dwell, delta, Npts=1000, *, fs=fs):
+        self._mode = 'idle'
         self._encoder = encoder
 #        self._xspress3 = xspress3
         self.start = float(start)
@@ -30,30 +36,41 @@ class SRXFlyer1Axis:
         self.npts = Npts
         self.speed = float(self.incr / self.dwell)
         self.delta = delta
-        
-        self._encoder.enc_pos1_sync = 1 
 
-    def stage(self):
         # X speed
         self.stage_sigs[self._motor.velocity] = self.speed
         # gating info for encoder capture
         self.stage_sigs[self._encoder.gate_start] = self.start
         self.stage_sigs[self._encoder.gate_width] = self.extent
-        self.stage_sigs[self._encoder.gate_step] = self.incr
-        self.stage_sigs[self._encoder.gate_num] = self.npts
+        self.stage_sigs[self._encoder.gate_step] = self.extent 
+        self.stage_sigs[self._encoder.gate_num] = 1
+        self.stage_sigs[self._encoder.pulse_start] = 0
+        self.stage_sigs[self._encoder.pulse_width] = self.dwell-0.005
+        self.stage_sigs[self._encoder.pulse_step] = self.dwell
+        self.stage_sigs[self._encoder.pulse_num] = self.npts
+
         self.stage_sigs[self._encoder.arm] = 1
         #pc gate output is 30 for zebra.  use it to trigger xspress3 and I0
-        self.stage_sigs[self._encoder.output1] = 30
-        self.stage_sigs[self._encoder.output3] = 30
+        self.stage_sigs[self._encoder.output1] = 31
+        self.stage_sigs[self._encoder.output3] = 31
+        
+        self._encoder.enc_pos1_sync = 1 
 
+    def stage(self):
         super().stage()
 
-    def unstage(self):
-        super().unstage()
-        
+    def kickoff(self, xstart, xstop, xnum, dwell):
+        self._mode = 'kicked off'
+        self.gate_start.put(xstart)
+        self.gate_stop.put(xstop)
+        self.pulse_max.put(xnum)
+        self.gate_width.put(dwell)
+        self.arm.put(1)
+        return NullStatus()  # TODO Return a status object *first* and do the above asynchronously.
 
-    def kickoff(self):
+        # NOTHING BELOW IS EVER RUN.
         return_values = []
+        
 #        # set stage speed
 #        self._motor.velocity = self._speed
 #        #####need to reset this!!!####
@@ -73,44 +90,75 @@ class SRXFlyer1Axis:
         return_values.append(ret)
         return return_values
 
-    def describe_collect(self):
-        return OrderedDict() 
-    
-    def read_configuration(self):
-        return OrderedDict()
-    
-    def describe_configuration(self):
-        return OrderedDict()
-    
     def complete(self):
-        #depends on motor position and self._encoder.data_in_progress bool
-        #first, motor completes, then data download begins.
+        """
+        Call this when all needed data has been collected. This has no idea
+        whether that is true, so it will obligingly stop immediately. It is
+        up to the caller to ensure that the motion is actually complete.
+        """
+        self._mode = 'complete'
+        self.arm.put(0)  # sanity check; this should happen automatically
         return NullStatus()
     
     def collect(self):
-        # fetch data from Zebra
-        data = self._encoder.pc.data.get()
-        output = np.zeros((2, len(data[0])))
-        output[0] = np.array(data.time)
-        output[1] = np.array(data.enc1)
-        # will cache these to xf05id1 user nfs directory and copy them to the 
-        # data volume
-        userdir = '/nfs/xf05id1/.fs_cache'
-        data_path = os.path.join(userdir, str(datetime.date.today())
-        uid = save_ndarray(output,data_path)
-        resource_document = fsapi.insert_resource('npy', data_path)
-        datum_document = fsapi.insert_datum(resource_document, uid, {})
-        # need to register this with metadatastore as x values...
-        # how?
+        # Create records in the FileStore database.
+        filename = '{}.h5'.format(uuid.uuid4())
+        read_filepath = os.path.join(self.LARGE_FILE_DIRECTORY_READ_PATH, filename)
+        write_filepath = os.path.join(self.LARGE_FILE_DIRECTORY_WRITE_PATH, filename)
+        resource = fs.insert_resource('ZEBRA_HDF51', read_filepath)
+        time_datum_id = uuid.uuid4()
+        enc1_datum_id = uuid.uuid4()
+        fs.insert_datum(resource, time_datum_id, {'column': 'time'})
+        fs.insert_datum(resource, enc1_datum_id, {'column': 'enc1'})
+
+        # Write the file.
+        export_zebra_data(self._encoder, write_filepath)
+
+        # Yield a (partial) Event document. The RunEngine will put this
+        # into metadatastore, as it does all readings.
+        yield {'time': time.time(), 'seq_num': 1,
+               'data': {'time': time_datum_id,
+                        'enc1': enc1_datum_id},
+               'timestamps': {'time': time_datum_id,  # not a typo
+                              'enc1': time_datum_id}}
+        self._mode = 'idle'
     
     def stop(self):
         pass
 
+    def pause(self):
+        "Pausing in the middle of a kickoff nukes the partial dataset."
+        self.arm.put(0)
+        self._mode = 'idle'
+        self.unstage()
 
-def SRXFly(xstart=None,xstepsize=None,xpts=None,dwell=None,ystart=None,ystepsize=None,ypts=None,xs=xs,ion=current_preamp):
+    def resume(self):
+        self.stage()
+
+
+def export_zebra_data(zebra, filepath):
+    data = zebra.pc.data.get()
+    output = np.zeros((2, len(data[0])))
+    output[0] = np.array(data.time)
+    output[1] = np.array(data.enc1)
+    with h5py.File(filepath, 'w') as f:
+        r.write(output)
+
+
+def SRXFly(xstart=None,xstepsize=None,xpts=None,dwell=None,
+           ystart=None,ystepsize=None,ypts=None,xs=xs,ion=current_preamp, md=None):
+    """
+
+    Monitor IO.
+    Zebra buffers x(t) points as a flyer.
+    Xpress3 is our detector.
+    The aerotech has the x and y positioners.
+    """
     delta = 0.01
     rows = np.linspace(ystart,ystart+(ypts-1)*ystepsize,ypts)
 
+    if md is None:
+        md = {}
     md = ChainMap(md, {
         'detectors': [zebra,xs],
         'x_range' : xstepsize*xpts,
@@ -119,16 +167,15 @@ def SRXFly(xstart=None,xstepsize=None,xpts=None,dwell=None,ystart=None,ystepsize
         }
     )
     try:
-        xs.external_trig = True
+        xs.external_trig.put(True)
         #set ion chamber to windowing mode
-        ion.trigger_mode = 5 
         yield from set_abs(hf_stage.x, xstart-delta, wait=True)
         yield from open_run(md)
         for n in rows:
             #xspress3 scan-specific set up
             xs.hdf5.capture = xpts
             #flyer = SRXFlyer(encoder, detectors, motor, start, incr, dwell, Npts=1000)
-            flyer = SRXFlyer(zebra, hf_stage.x, xstart, xstepsize, dwell, xpts, delta)
+            flyer = SRXFlyer(zebra, hf_stage.x, xstart, xstepsize, dwell, delta, xpts)
             flyer.stage()
             yield Msg('checkpoint')
             yield Msg('stage', xs)
@@ -148,5 +195,52 @@ def SRXFly(xstart=None,xstepsize=None,xpts=None,dwell=None,ystart=None,ystepsize
             #run-specific metadata?
         yield from close_run()
     finally:
-        xs.external_trig = False
-        ion.trigger_mode = 2
+        xs.external_trig.put(False)
+
+
+from bluesky.plans import (one_1d_step, kickoff, collect, complete, scan
+                           monitor_during_wrapper, abs_set)
+
+
+def scan_and_fly(xstart, xstop, xnum, ystart, ystop, ynum, dwell, *,
+                 delta=None,
+                 xmotor=hf_stage.x, ymotor=hf_stage.y,
+                 xs=xs, ion=current_preamp,
+                 flying_zebra=flying_zebra, md=None):
+    """
+
+    Monitor IO.
+    Zebra buffers x(t) points as a flyer.
+    Xpress3 is our detector.
+    The aerotech has the x and y positioners.
+    """
+    if md is None:
+        md = {}
+    if delta is None:
+        delta=0.01
+    md = ChainMap(md, {
+        'detectors': [zebra,xs],
+        'x_range' : xstepsize*xpts,
+        'dwell' : dwell,
+        'y_range' : ystepzie*ypts,
+        }
+    )
+
+    def fly_each_step(detectors, motor, step):
+        # First, let 'scan' handle the normal y step, including a checkpoint.
+        yield from one_1d_step(detectors, motor, step)
+
+        # Now do the x steps.
+        yield from abs_set(xmotor, xstart - delta, wait=True) # ready to move
+        v = (xstop - xstart) / xnum / dwell  # compute "stage speed"
+        yield from abs_set(xmotor.velocity, v)  # set the "stage speed"
+        # arm the Zebra (start caching x positions)
+        yield from kickoff(flying_zebra, xstart, xstop, xnum)
+        yield from abs_set(xmotor, stop, wait=True)  # move in x
+        yield from complete(flying_zebra)  # tell the Zebra we are done
+        yield from collect(flying_zebra)  # extract data from Zebra
+
+    @monitor_during_decorator([ion, xs])  # monitor values from ion and acquisitions from xs
+    @stage_decorator([xmotor, flying_zebra, xs, ion])  # Below, 'scan' stage ymotor.
+    def plan():
+        return (yield from scan([], ymotor, ystart, ystop, ynum, per_step=fly_each_step, md=md))
