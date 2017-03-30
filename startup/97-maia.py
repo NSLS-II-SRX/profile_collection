642c8fff65faed4ab4452f92bcd8af2b60cc6fd1
SRX Operator <xf05id1@xf05id1-ws4.cs.nsls2.local>
2017-03-30 14:39:40 -0400

    work on maia

--- startup/97-maia.py
+++ startup/97-maia.py
@@ -7,17 +7,6 @@ from collections import OrderedDict
 import time as ttime
 
 
-x = Mover('x', OrderedDict([('x', lambda x: x),
-                            ('x_setpoint', lambda x: x)]),
-          {'x': 0}, conf={'velocity': 3})
-
-y = Mover('y', OrderedDict([('y', lambda y: y),
-                            ('y_setpoint', lambda y: y)]),
-          {'y': 0}, conf={'velocity': 3})
-
-shutter = Mover('shutter', {'shutter': lambda shutter: shutter},
-                {'shutter': 'closed'}, conf={'velocity': 3})
-
 
 class EncoderAxis(Device):
     pos = Cpt(Signal)
@@ -28,62 +17,43 @@ class Encoder(Device):
     y = Cpt(EncoderAxis, 'x')
 
 
-class MAIA(Device):
-    x_motor = None  # EpicsMotor
-    y_motor = None  # EpicsMotor
-    blog = None  # metadata log
-    encoder = Cpt(Encoder, '')  # complex object, values copied from motors
-    pixel = None  # pixel shape, pitch info, origin
-    scan = None  # metadata about scan (dwell time, scan order)
-    pixel_enable = None   # bool
-    pixel_event_enable = None  # bool
-
-    def __init__(self, *args, conf, **kwargs):
+class MAIA(Kandinskivars):
+    fly_keys = ['blog.info.blogd_data_path',
+                'blog.info.blogd_working_directory',
+                'blog.info.run_number']
+    def __init__(self,*args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._conf = conf
-
-    def configure(self, d):
-        old = dict(self._conf)
-        self._conf.update(d)
-        return old, dict(self._conf)
-
-    def read_configuration(self):
-        return {k: {'value': v, 'timestamp': ttime.time()}
-                for k, v in self._conf.items()}
-
-    @property
-    def conf_attrs(self):
-        return list(self._conf_state)
-
-    def stage(self):
-        ...
-        # copy info from motors -> encoder
-        # set pixel information
-        # set scan information
-
-    def unstage(self):
-        ...
 
     def kickoff(self):
-        ...
-        # enable blogging
-        # enable pixels
-        # enable pixel event
-        # blog.group.next
-        # set blog_enabled
-        return NullStatus()
+        # paranoia enableing
+        self.maia.pixel_enable_cmd.set(True)
+        self.maia.pixel_event_enable_cmd.set(True)
+        self.photon_enable_sp.set(1)
+        # this is the one that matters
+        return self.blog.newrun.set(1)
 
     def complete(self):
-        return NullStatus()
+        return self.blog.endrun.set(1)
 
     def collect(self):
-        return []
+        vals = OrderedDict()
+        for k in self.fly_keys:
+            vals.update(getattr(self, k).read())
+        
+        data = {k: v['value'] for k, v in vals.items()}
+        ts = {k: v['timestamp'] for k, v in vals.items()}
+        yield {'data': data, 'timestamps': ts,
+               'time': ttime.time(), 'seq_num': 0}
 
     def describe_collect(self):
-        return {}
+        descs = OrderedDict()
+        for k in self.fly_keys:
+            descs.update(getattr(self, k).describe())
+            
+        return {'primary': descs}
 
 
-maia = MAIA('srx:maia', name='maia', conf={})
+# maia = MAIA('SRX:MAIA', name='maia')
 
 
 def first_key_plan(pos):
@@ -94,62 +64,114 @@ def first_key_plan(pos):
 
 
 def fly_maia(xstart, xstop, xnum,
-             ystart, ystop, ynum, *,
+             ystart, ystop, ynum, 
+             dwell, *,
              md=None):
+    shutter = shut_fast
     if md is None:
         md = {}
+        
+    md = dict(md)
+    
+    md.update(xstart=xstart, xstop=xstop,
+              xnum=xnum, ynum=ynum, dwell=dwell)
+    
+    if xstart > xstop:
+        xstop, xtart = xstart, xstop
+
+    if ystart > ystop:
+        ystop, ytart = ystart, ystop
+        
     x_pitch = abs(xstop - xstart) / xnum
     y_pitch = abs(ystop - ystart) / ynum
 
     # TODO compute this based on someting
-    ssr_x = 5
-    ssr_y = 5
+    spd_x = (x_pitch / dwell) 
 
-    yield from bp.abs_set(x, xstart, group='maia_sync')
-    yield from bp.abs_set(y, ystart, group='maia_sync')
+    yield from bp.abs_set(hf_stage.x, xstart, group='maia_sync')
+    yield from bp.abs_set(hf_stage.y, ystart, group='maia_sync')
 
     yield from bp.wait(group='maia_sync')
 
-    maia_old_c, mia_new_c = yield from bp.configure(
+    cf_ret = yield from bp.configure(
         maia, {'x.pitch': x_pitch, 'y.pitch': y_pitch})
+    if cf_ret is not None:
+        maia_old_c, mia_new_c = cf_ret
+    else:
+        maia_old_c, mia_new_c = {}, {}
 
     # make sure the maia agrees with where the motors are
     # this is important because it works off of dead-reckoning to know
     # where it is (?)  this is not configuration
 
-    x_val = yield from first_key_plan(x)
-    y_val = yield from first_key_plan(x)
+    x_val = yield from first_key_plan(hf_stage.x)
+    y_val = yield from first_key_plan(hf_stage.y)
     # TODO, depends on actual device
-    yield from bp.mv(maia.encoder.x.pos, x_val)
-    yield from bp.mv(maia.encoder.y.pos, y_val)
+    yield from bp.mv(maia.enc_axis_0_pos_sp, x_val)
+    yield from bp.mv(maia.enc_axis_1_pos_sp, y_val)
+
+    yield from bp.mv(maia.x_pixel_dim_origin_sp, xstart)
+    yield from bp.mv(maia.y_pixel_dim_origin_sp, ystart)
+    
+    yield from bp.mv(maia.x_pixel_dim_pitch_sp, x_pitch)
+    yield from bp.mv(maia.y_pixel_dim_pitch_sp, y_pitch)
+    
+    yield from bp.mv(maia.x_pixel_dim_coord_extent_sp, xnum)
+    yield from bp.mv(maia.y_pixel_dim_coord_extent_sp, ynum)
+    yield from bp.mv(maia.scan_order_sp, '01')
+    yield from bp.mv(maia.pixel_dwell, dwell)
+    
+    #    yield from bp.mv(maia.maia_scan_info
+    #need something to generate a filename here.
+    #    yield from bp.mv(maia.blog_group_next_sp,datafile))
+    #start blog in kickoff?
 
     # set the motors to the right speed
-    # TODO reset these
-    old_x_c, _ = yield from bp.configure(x, {'velocity': ssr_x})
-    old_y_c, _ = yield from bp.configure(y, {'velocity': ssr_y})
+    cf_ret = yield from bp.configure(hf_stage.x, {'velocity': spd_x})
+    if cf_ret is not None:
+        old_x_c = cf_ret[0]
+    else:
+        old_x_c = {}
+    #old_y_c, _ = yield from bp.configure(y, {'velocity': ssr_y})
 
-    yield from bp.mv(shutter, 'open')
-    yield from bp.open_run(md)
-    yield from bp.stage(maia)
-    yield from bp.kickoff(maia)
-    # by row
-    for i, x_pos in enumerate(np.linspace(xstart, xstop, xnum)):
-        # move to the row we want
-        yield from bp.mv(x, x_pos)
-        if i % 2:
-            # for odd-rows move from start to stop
-            yield from bp.mv(y, ystop)
-        else:
-            # for even-rows move from stop to start
-            yield from bp.mv(y, ystart)
-    # stop the maia ("I'll wait until your done")
-    yield from bp.complete(maia)
-    # shut the stutter
-    yield from bp.mv(shutter, 'close')
-    # collect data from maia
-    yield from bp.collect(maia)
-    yield from bp.close_run()
+    yield from bp.mv(shutter, 'Open')
+    # TODO shove this into the maia md
+    maia_cur_run = yield from first_key_plan(maia.blog_runno_mon)
+    if maia_cur_run is not None:
+        maia_next_run = maia_next_run + 1
+    else:
+        maia_next_run = -1
+    md['maia_runno'] = maia_next_run
+    start_uid = yield from bp.open_run(md)
+    #long int here.  consequneces of changing?
+#    yield from bp.mv(maia.scan_number_sp,start_uid)
+    yield from bp.stage(maia)  # currently a no-op
+    
+    def _raster_plan():
+        yield from bp.kickoff(maia, wait=True)
+        # by row
+        for i, y_pos in enumerate(np.linspace(ystart, ystop, ynum)):
+            # move to the row we want
+            yield from bp.mv(hf_stage.y, y_pos)
+            if i % 2:
+                # for odd-rows move from start to stop
+                yield from bp.mv(hf_stage.x, xstop)
+            else:
+                # for even-rows move from stop to start
+                yield from bp.mv(hf_stage.x, xstart)
+                
+    def _cleanup_plan():
+        # stop the maia ("I'll wait until you're done")
+        yield from bp.complete(maia, wait=True)
+        # shut the shutter
+        yield from bp.mv(shutter, 'Close')
+        # collect data from maia
+        yield from bp.collect(maia)
+    
+        yield from bp.unstage(maia)
+        yield from bp.close_run()
 
-    yield from bp.configure(x, old_x_c)
-    yield from bp.configure(y, old_y_c)
-    yield from bp.configure(maia, maia_old_c)
+        yield from bp.configure(hf_stage.x, old_x_c)
+        yield from bp.configure(maia, maia_old_c)
+
+    
