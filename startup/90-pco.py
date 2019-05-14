31c9af6a8c99bbe2b2c11fc3e780c48ce88cba1a
Thomas A Caswell <tcaswell@bnl.gov>
2019-05-14 14:34:45 -0400

    FIX: override get_frames_per_point

--- startup/90-pco.py
+++ startup/90-pco.py
@@ -4,8 +4,13 @@ from ophyd.areadetector.plugins import ROIPlugin, StatsPlugin, ImagePlugin
 from ophyd import Component as C
 from ophyd.areadetector.filestore_mixins import FileStoreHDF5IterativeWrite
 
+
 class HDF5PluginWithFileStore(HDF5Plugin, FileStoreHDF5IterativeWrite):
     file_number_sync = None
+
+    def get_frames_per_point(self):
+        return self.parent.cam.num_images.get()
+
 
 class SRXPCOEDGECam(SingleTrigger, AreaDetector):
     cam = C(AreaDetectorCam, 'cam1:')
@@ -20,8 +25,9 @@ class SRXPCOEDGECam(SingleTrigger, AreaDetector):
     roi3 = C(ROIPlugin, 'ROI3:')
     roi4 = C(ROIPlugin, 'ROI4:')
     hdf = C(HDF5PluginWithFileStore, 'HDF1:',
-            read_path_template='/data/PCOEDGE/%Y/%m/%d/',
-            write_path_template='C:\epicsdata\pcoedge\debug_testing',
+            read_path_template=r'/data/PCOEDGE/%Y/%m/%d/',
+            write_path_template=r'C:\epicsdata\pcoedge\debug_testing',
             root='/data')
 
-pcoedge = SRXPCOEDGECam('XF05IDD-ES{PCO:1}:',name='pcoedge')
+
+pcoedge = SRXPCOEDGECam('XF05IDD-ES{PCO:1}:', name='pcoedge')
