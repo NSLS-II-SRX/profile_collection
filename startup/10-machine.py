97a536f04da7c58ff183c99b5ed4386a36bcdb61
Thomas A Caswell <tcaswell@bnl.gov>
2019-07-26 18:25:24 -0400

    STY: whitespace

--- startup/10-machine.py
+++ startup/10-machine.py
@@ -534,34 +534,34 @@ _time_fmtstr = '%Y-%m-%d %H:%M:%S'
 #     # the vlaue comingout of the PV do not match what is shown in CSS
 #     open_cmd = Cpt(EpicsSignal, 'Cmd:Opn-Cmd', string=True)
 #     open_val = 'Open'
-# 
+#
 #     close_cmd = Cpt(EpicsSignal, 'Cmd:Cls-Cmd', string=True)
 #     close_val = 'Not Open'
-# 
+#
 #     status = Cpt(EpicsSignalRO, 'Pos-Sts', string=True)
-# 
+#
 #     close_status = Cpt(EpicsSignalRO, 'Sts:Cls-Sts')
 #     fail_to_close = Cpt(EpicsSignalRO, 'Sts:FailCls-Sts', string=True)
 #     fail_to_open = Cpt(EpicsSignalRO, 'Sts:FailOpn-Sts', string=True)
 #     # user facing commands
 #     open_str = 'Open'
 #     close_str = 'Close'
-# 
+#
 #     def set(self, val):
 #         if self._set_st is not None:
 #             raise RuntimeError('trying to set while a set is in progress')
-# 
+#
 #         cmd_map = {self.open_str: self.open_cmd,
 #                    self.close_str: self.close_cmd}
 #         target_map = {self.open_str: self.open_val,
 #                       self.close_str: self.close_val}
-# 
+#
 #         cmd_sig = cmd_map[val]
 #         target_val = target_map[val]
-# 
+#
 #         st = self._set_st = DeviceStatus(self)
 #         enums = self.status.enum_strs
-# 
+#
 #         def shutter_cb(value, timestamp, **kwargs):
 #             value = enums[int(value)]
 #             if value == target_val:
@@ -571,7 +571,7 @@ _time_fmtstr = '%Y-%m-%d %H:%M:%S'
 #         uid = str(uuid.uuid4())
 #         cmd_enums = cmd_sig.enum_strs
 #         count = 0
-# 
+#
 #         def cmd_retry_cb(value, timestamp, **kwargs):
 #             nonlocal count
 #             value = cmd_enums[int(value)]
@@ -588,12 +588,12 @@ _time_fmtstr = '%Y-%m-%d %H:%M:%S'
 #                     cmd_sig.set(1)
 #                 else:
 #                     cmd_sig.clear_sub(cmd_retry_cb)
-# 
+#
 #         cmd_sig.subscribe(cmd_retry_cb, run=False)
 #         cmd_sig.set(1)
 #         self.status.subscribe(shutter_cb)
 #         return st
-# 
+#
 #     def __init__(self, *args, **kwargs):
 #         super().__init__(*args, **kwargs)
 #         self._set_st = None
