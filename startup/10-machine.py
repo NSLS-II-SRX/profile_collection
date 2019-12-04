85c6038bec07634388b62c41b9811c33e2445bac
Andy Kiss <akiss@bnl.gov>
2019-12-04 17:03:46 -0500

    Renamed 15-optics to 11-optics to help with spreading out file names Removed legacy optics (high-flux mirrors) Rearranged code to load in order from source to detector Removed old, commented out code

--- startup/10-machine.py
+++ startup/10-machine.py
@@ -7,31 +7,36 @@ from ophyd.ophydobj import MoveStatus
 from ophyd.pseudopos import (pseudo_position_argument, real_position_argument)
 from ophyd.positioner import PositionerBase
 from ophyd import Component as Cpt
-from nslsii.devices import TwoButtonShutter
 
 from scipy.interpolate import InterpolatedUnivariateSpline
 import functools
 import math
 import uuid
 
+# For organization, this file will define objects for the machine. This will
+# include the undulator (and energy axis) and front end slits.
 
+
+### Constants
+ANG_OVER_EV = 12.3984
+
+
+### Signals
 ring_current = EpicsSignalRO('SR:C03-BI{DCCT:1}I:Real-I', name='ring_current')
+# Move this to cryo?
 cryo_v19 = EpicsSignal('XF:05IDA-UT{Cryo:1-IV:19}Sts-Sts', name='cryo_v19')
 
 
+# Is this used?
 _undulator_kwargs = dict(name='ivu1_gap', read_attrs=['readback'],
                          calib_path='/nfs/xf05id1/UndulatorCalibration/',
-                         # calib_file='SRXUgapCalibration20150411_final.text',
-                         # calib_file='SRXUgapCalibration20160608_final.text',
-                         # calib_file='SRXUgapCalibration20170131.txt',
                          calib_file='SRXUgapCalibration20170612.txt',
                          configuration_attrs=['corrfunc_sta', 'pos', 'girder',
                                               'real_pos', 'elevation'])
 
 
-ANG_OVER_EV = 12.3984
 
-
+### Setup undulator
 class InsertionDevice(Device, PositionerBase):
     gap = Cpt(EpicsMotor, '-Ax:Gap}-Mtr',
               kind='hinted', name='')
@@ -116,10 +121,11 @@ class InsertionDevice(Device, PositionerBase):
         return self.gap.subscribe(inner, *args, **kwargs)
 
 
+### Setup energy axis
 class Energy(PseudoPositioner):
-    # synthetic axis
+    # Synthetic axis
     energy = Cpt(PseudoSingle)
-    # real motors
+    # Real motors
     u_gap = Cpt(InsertionDevice, 'SR:C5-ID:G1{IVU21:1')
     bragg = Cpt(EpicsMotor, 'XF:05IDA-OP:1{Mono:HDCM-Ax:P}Mtr', add_prefix=(),
                 read_attrs=['user_readback'])
@@ -128,15 +134,16 @@ class Energy(PseudoPositioner):
     epics_d_spacing = EpicsSignal('XF:05IDA-CT{IOC:Status01}DCMDspacing.VAL')
     epics_bragg_offset = EpicsSignal('XF:05IDA-CT{IOC:Status01}BraggOffset.VAL')
 
-    # motor enable flags
+    # Motor enable flags
     move_u_gap = Cpt(Signal, None, add_prefix=(), value=True)
     move_c2_x = Cpt(Signal, None, add_prefix=(), value=True)
     harmonic = Cpt(Signal, None, add_prefix=(), value=0, kind='config')
     selected_harmonic = Cpt(Signal, None, add_prefix=(), value=0)
 
+    # Is this duplicated?
     c2pitch_kill = EpicsSignal("XF:05IDA-OP:1{Mono:HDCM-Ax:P2}Cmd:Kill-Cmd")
 
-    # experimental
+    # Experimental
     detune = Cpt(Signal, None, add_prefix=(), value=0)
 
     def energy_to_positions(self, target_energy, undulator_harmonic, u_detune):
@@ -161,7 +168,7 @@ class Energy(PseudoPositioner):
              The angle to set the monocromotor
 
         """
-        # set up constants
+        # Set up constants
         Xoffset = self._xoffset
         d_111 = self._d_111
         delta_bragg = self._delta_bragg
@@ -169,13 +176,13 @@ class Energy(PseudoPositioner):
         T2cal = self._t2cal
         etoulookup = self.etoulookup
 
-        # calculate Bragg RBV
+        # Calculate Bragg RBV
         BraggRBV = (
             np.arcsin((ANG_OVER_EV / target_energy) / (2 * d_111)) /
             np.pi * 180 -
             delta_bragg)
 
-        # calculate C2X
+        # Calculate C2X
         Bragg = BraggRBV + delta_bragg
         T2 = (Xoffset *
               np.sin(Bragg * np.pi / 180) /
@@ -183,7 +190,7 @@ class Energy(PseudoPositioner):
         dT2 = T2 - T2cal
         C2X = C2Xcal - dT2
 
-        # calculate undulator gap
+        # Calculate undulator gap
 
         #  TODO make this more sohpisticated to stay a fixed distance
         #  off the peak of the undulator energy
@@ -202,8 +209,6 @@ class Energy(PseudoPositioner):
         harmonic : int, optional
             The harmonic to use, defaults to 3
         """
-#        uga    scanlogDic = {'Fe': 11256, 'Cu':11254, 'Cr': 11258, 'Ti': 11260, 'Se':11251}
-        # scanlogDic = {'Fe':11369, 'Cu':11367, 'Ti':11371, 'Se':11364}
         p = self.u_gap.get().readback
         utoelookup = self.utoelookup
 
@@ -242,7 +247,8 @@ class Energy(PseudoPositioner):
 
 
     def crystal_gap(self):
-        """Return the current physical gap between first and second crystals
+        """
+        Return the current physical gap between first and second crystals
         """
         C2X = self.c2_x.get().user_readback
         bragg = self.bragg.get().user_readback
@@ -272,16 +278,13 @@ class Energy(PseudoPositioner):
         if energy <= 4.4:
             raise ValueError("The energy you entered is too low ({} keV). "
                              "Minimum energy = 4.4 keV".format(energy))
-#        if energy >= 25:
-#            raise ValueError('The energy you entered is too high ({} keV). '
-#                             'Maximum energy = 25.0 keV'.format(energy))
         if (energy > 25.):
             if (energy < 4400.) or (energy > 25000.):
-            #energy is invalid
+            # Energy is invalid
                 raise ValueError('The requested photon energy is invalid ({} keV). '
                              'Values must be in the range of 4.4 - 25 keV'.format(energy))
             else:
-            #energy is in eV
+            # Energy is in eV
                 energy = energy / 1000.
 
         # harmonic cannot be None, it is an undesired datatype
@@ -301,14 +304,14 @@ class Energy(PseudoPositioner):
                     break
                 harmonic += 2
         self.selected_harmonic.put(harmonic)
-        # compute where we would move everything to in a perfect world
+        # Compute where we would move everything to in a perfect world
         bragg, c2_x, u_gap = self.energy_to_positions(energy, harmonic, detune)
 
-        # sometimes move the crystal gap
+        # Sometimes move the crystal gap
         if not self.move_c2_x.get():
             c2_x = self.c2_x.position
 
-        # sometimes move the undulator
+        # Sometimes move the undulator
         if not self.move_u_gap.get():
             u_gap = self.u_gap.position
 
@@ -329,188 +332,16 @@ class Energy(PseudoPositioner):
         self.epics_bragg_offset.put(self._delta_bragg)
 
 
-# change it to a better way to pass the calibration
-cal_data_2016cycle1 = {'d_111': 3.12961447804,
-                       'delta_bragg': 0.322545952931,
-                       'C2Xcal': 3.6,
-                       'T2cal': 13.463294326,
-                       'xoffset': 25.2521}
-
-cal_data_2016cycle1_2 = {'d_111': 3.12924894907,  # 2016/1/27 (Se, Cu, Fe, Ti)
-                       'delta_bragg': 0.315532509387,  # 2016/1/27 (Se, Cu, Fe, Ti)
-                       'delta_bragg': 0.317124613301,  # ? before 8/18/2016
-                       #'delta_bragg': 0.357124613301,  # 2016/8/16 (Cu)
-                       # not in energy axis but for the record
-                       # 'C1Rcal' :  -4.88949983261, # 2016/1/29
-                       'C2Xcal': 3.6,  # 2016/1/29
-                       'T2cal': 13.7187120636,
-                       #'xoffset': 25.01567531283996 #2016 Jan
-                       #'xoffset': 24.756374595028607 #2016/2/25 on Rh stripe, y=10 mm
-                       #'xoffset': 24.908823293008666 #2016/2/26 on Si stripe, y=5 mm
-                       #'xoffset': 24.621311485825125 #2016/3/10 Fe edge
-                       #'xoffset': 24.661899615539824 #2016/3/13 Pt edge
-                       #'xoffset': 24.761023845083287 #2016/3/17 Zr edge, 18.2 keV
-                       #'xoffset': 24.741174927854445 #2016/3/23 15 keV
-                       #'xoffset': 24.593840056028178 #2016/3/26 10.5 keV W
-                       #'xoffset': 24.773110163531658 #2016/3/26 18.0 keV U
-                       #'xoffset':  24.615016005680289 #2016/4/06 12.8 keV Se
-                       #'xoffset': 24.672213516710034
-                       #'xoffset': 24.809807128906538
-                       #mono warmed up at 4/12/16 0.494304059171
-                       #'xoffset': 24.809838976060604 #17keV
-                       #'xoffset': 24.887490886653893 #8.2 keV
-                       #'xoffset': 24.770168843970197 #12.5 keV
-                       }  # 2016/1/29}
-
-                        #2016-2
-cal_data_2016cycle2  ={ #'d_111': 3.13130245128, #2016/6/9 (Ti, Cr, Fe, Cu, Se)
-                        'd_111': 3.12929567478, #2016/8/1 (Ti, Fe, Cu, Se)
-                        #'delta_bragg' : 0.309366522013,
-                        #'delta_bragg' : 0.32936652201300004,
-                        #'delta_bragg': 0.337124613301,
-                        'delta_bragg': 0.317124613301, #2016/8/16 (Ti, Cr, Fe, Cu, Se)
-                        #'xoffset': 24.864494684263519,
-                        'C2Xcal': 3.6,  # 2016/1/29
-                        'T2cal': 14.2470486188,
-                        #'xoffset': 25.941277803299684
-                        #'xoffset': 25.921698318063775
-                        #'xoffset': 25.802588306223701 #2016/7/5 17.5 keV
-                        #'xoffset': 25.542954465467645 #2016/7/6 17.5 keV fullfield
-                        #'xoffset': 25.39464922124886 #2016/7/20 13.5/6 keV microscopy
-                        #'xoffset': 25.354968816872358 #2016/7/28 12-14 keV microscopy
-                        #'xoffset': 25.414219669872864 #2016/8/2 14 keV
-                        #'xoffset': 25.175826062860775 #2016/8/2 18 keV
-                        'xoffset': 25.527059255709876 #2016/8/16 9 keV
-                        #'xoffset': 25.487723997622723 #2016/8/18 11 keV
-                        #'xoffset': 25.488305806234468, #2016/8/21 9.2 keV, aligned by Garth on 2016/8/20
-                        #'C1Rcal':-4.7089492561 for the record
-                      }
-
-cal_data_2016cycle3  ={'d_111': 3.12941028109, #2016/10/3 (Ti, Fe, Cu, Se)
-                       'delta_bragg': 0.317209816326, #2016/10/3 (Ti, Fe, Cu, Se)
-                        'C2Xcal': 3.6,  # 2016/1/29
-                        'T2cal': 14.2470486188,
-#                        'xoffset': 25.056582386746765, #2016/10/3 9 keV
-#                        'xoffset': 25.028130552150312, #2016/10/12 8 keV
-#                        'xoffset': 25.182303347383915, #2016/10/24 7.4 keV
-#                        'xoffset': 25.531497575767418, #2016/10/24 7.4 keV
-                        'xoffset': 25.491819462118674, #2016/11/4 12.8 keV
-                        #'C1Rcal': -5.03023390228  #for the record, 2016/10/3
-                      }
-
-
-cal_data_2017cycle1  ={#'d_111': 3.12988412345, #2017/1/17 (Ti, Cr, Fe, Cu, Se)
-#                        'd_111': 3.13246886211,
-#                        'delta_bragg': 0.298805934621, # Cu, Se 3/17/2017
-#                        'd_111': 3.11630891423,
-#                        'delta_bragg': 0.357259819067, # {'Cu':3941, 'Se':3940, 'V':3937, 'Ti':3946, 'Mn':3951} 3/23/2017
-                        'd_111': 3.12969964541,
-                        'delta_bragg': 0.306854237528, # {'Ni':4156, 'Co':4154, 'Mn':4152} 4/10/2017
-#                        'd_111': 3.14434282312,
-#                        'delta_bragg': 0.265291543541, # {'Se':4002, 'Ti':4004, 'Mn':4000} 4/05/2017 bad
-#                       'delta_bragg': 0.314906135851, #2017/1/17 (Ti, Cr, Fe, Cu, Se)
-#                       'delta_bragg': 0.33490613585100004, #2017/3/9 (peakup spectrum)
-#                       'delta_bragg': 0.309906135851, #2017/3/17 (Cu)
-                        'C2Xcal': 3.6,  # 2017/1/17
-                        'T2cal': 15.0347755916,  # 2017/1/17
-                        'xoffset': 25.4253705456081, #2017/1/17 12.6 keV
-                        #'C1Rcal': -4.98854110244  #for the record, #2017/1/17
-                      }
-cal_data_2017cycle2 = {
- 'd_111': 3.12894833524,
- 'delta_bragg': 0.309484915727, #{'Ti':4843,'Mn':4845,'Cu':4847,'Se':4848} 20170525
- 'C2Xcal': 3.6,
- 'T2cal': 15.0347755916,
- 'xoffset': 24.581644618999363, #value for Ti worked best using the C2 pitch to correct at higher E
-}
-cal_data_2017cycle3 = {
- 'd_111': 3.10752771302,
- 'delta_bragg': 0.352619283445, #{'Ti':7959,'V':7961,'Mn':7963,'Cu':7964,'Se':7965} 201700928
- 'C2Xcal': 3.6,
- 'T2cal': 15.0347755916,
- 'xoffset': 24.581644618999363, #value for Ti worked best using the C2 pitch to correct at higher E
-}
-"""
-cal_data_2018cycle1 = {
- 'd_111': 3.03796349212,
- 'delta_bragg': 0.494304059171, #{'Ti':11371,'Fe':11369,'Cu':11367,'Se':11364} 201800203
- 'C2Xcal': 3.6,
- 'T2cal': 15.0347755916,
- 'xoffset': 24.581644618999363, #value for Ti worked best using the C2 pitch to correct at higher E
-}
-"""
-cal_data_2018cycle1 = {
- 'd_111': 3.12949171228,
- 'delta_bragg': 0.309950475093, #{'Ti':11371,'Fe':11369,'Cu':11367,'Se':11364} 201800203
- 'C2Xcal': 3.6,
- 'T2cal': 15.0347755916,
- 'xoffset': 24.581644618999363, #value for Ti worked best using the C2 pitch to correct at higher E
-}
-cal_data_2018cycle1a = {
- 'd_111': 3.1263, #changed by hand after DCM calibration changed; assume that heating drives this and not angle offset
- 'delta_bragg': 0.309950475093, #{'Ti':11371,'Fe':11369,'Cu':11367,'Se':11364} 201800203
- 'C2Xcal': 3.6,
- 'T2cal': 15.0347755916,
- 'xoffset': 24.75, #best value for 12 and 5 keV
-}
-cal_data_2018cycle1b = {
- 'd_111': 3.1291350735, #changed by hand after DCM calibration changed; assume that heating drives this and not angle offset
- 'delta_bragg': 0.309886328328, #{'Ti':12195,'Fe':12194,'Se':12187} 20180224
- 'C2Xcal': 3.6,
- 'T2cal': 15.0347755916,
- 'xoffset': 24.75, #best value for 12 and 5 keV
-}
-
-cal_data_2018cycle2 = {
- 'd_111': 3.128549107739033,
- 'delta_bragg': 0.3139487894740349, # {'Fe':14476, 'V':14477, 'Cr':14478, 'Cu':14480, 'Se':14481, 'Zr':14482}
- 'C2Xcal': 3.6,
- 'T2cal': 15.0347755916,
- 'xoffset': 24.75, #best value for 12 and 5 keV
-}
-
-cal_data_2018cycle3 = {
- 'd_111': 3.1292294240934786,
- 'delta_bragg': 0.3113245678165956, # {'V':18037, 'Cr':18040, 'Fe':18043, 'Cu':18046, 'Se':18049, 'Zr':18052}
- 'C2Xcal': 3.6,
- 'T2cal': 15.0347755916,
- 'xoffset': 24.75, #best value for 12 and 5 keV
-}
-
-cal_data_2019cycle1 = {
- 'd_111': 3.129024799425239,
- 'delta_bragg': 0.3092257938019577, # {'V':21828, 'Cr':21830, 'Fe':21833, 'Cu':21835, 'Se':21838, 'Zr':21843}
- 'C2Xcal': 3.6,
- 'T2cal': 15.0347755916,
- 'xoffset': 24.465, #best value for 12 and 5 keV
-}
-
-cal_data_2019cycle1 = {
- 'd_111': 3.143196587210581,
- 'delta_bragg': 0.2011442467795649,
- 'C2Xcal': 3.6,
- 'T2cal': 15.0347755916,
- 'xoffset': 24.646398991691104 #best value for 12 and 5 keV
-}
-
-cal_data_2019cycle1 = {
- 'd_111': 3.128774072188798,
- 'delta_bragg': 0.22324196449806297,
- 'C2Xcal': 3.6,
- 'T2cal': 15.0347755916,
- # 'xoffset': 24.646398991691104 #best value for 12 and 5 keV
- 'xoffset': 24.770  # not sure why value changed midcycle, reoptimized at 5 and 12 keV
-}
-
+# Let's try to only keep one "old" value in for ease of use
 # Calibrated 2019-08-16
 # Use scans 29496 - 29503
-cal_data_2019cycle3 = {
- 'd_111': 3.1287603762011367,
- 'delta_bragg': 0.21004620069236488,
- 'C2Xcal': 3.6,
- 'T2cal': 15.0347755916,
- 'xoffset': 24.770
-}
+# cal_data_2019cycle3 = {
+#  'd_111': 3.1287603762011367,
+#  'delta_bragg': 0.21004620069236488,
+#  'C2Xcal': 3.6,
+#  'T2cal': 15.0347755916,
+#  'xoffset': 24.770
+# }
 
 # Calibrated 2019-09-12
 # Use scans 30612 - 30617
@@ -522,96 +353,22 @@ cal_data_2019cycle3 = {
  'xoffset': 24.770
 }
 
-
 energy = Energy(prefix='', name='energy', **cal_data_2019cycle3)
 energy.synch_with_epics()
 energy.value = 1.0
 
 
-# Front End Slits (Primary Slits)
+### Setup front end slits (primary slits)
 class SRXSlitsFE(Device):
     top = Cpt(EpicsMotor, '3-Ax:T}Mtr')
     bot = Cpt(EpicsMotor, '4-Ax:B}Mtr')
     inb = Cpt(EpicsMotor, '3-Ax:I}Mtr')
     out = Cpt(EpicsMotor, '4-Ax:O}Mtr')
 
-
 fe = SRXSlitsFE('FE:C05A-OP{Slt:', name='fe')
 
+
+# Is this needed here? Can it be moved to 00-setup?
 _time_fmtstr = '%Y-%m-%d %H:%M:%S'
 
 
-# class TwoButtonShutter(Device):
-#     # TODO this needs to be fixed in EPICS as these names make no sense
-#     # the vlaue comingout of the PV do not match what is shown in CSS
-#     open_cmd = Cpt(EpicsSignal, 'Cmd:Opn-Cmd', string=True)
-#     open_val = 'Open'
-# 
-#     close_cmd = Cpt(EpicsSignal, 'Cmd:Cls-Cmd', string=True)
-#     close_val = 'Not Open'
-# 
-#     status = Cpt(EpicsSignalRO, 'Pos-Sts', string=True)
-# 
-#     close_status = Cpt(EpicsSignalRO, 'Sts:Cls-Sts')
-#     fail_to_close = Cpt(EpicsSignalRO, 'Sts:FailCls-Sts', string=True)
-#     fail_to_open = Cpt(EpicsSignalRO, 'Sts:FailOpn-Sts', string=True)
-#     # user facing commands
-#     open_str = 'Open'
-#     close_str = 'Close'
-# 
-#     def set(self, val):
-#         if self._set_st is not None:
-#             raise RuntimeError('trying to set while a set is in progress')
-# 
-#         cmd_map = {self.open_str: self.open_cmd,
-#                    self.close_str: self.close_cmd}
-#         target_map = {self.open_str: self.open_val,
-#                       self.close_str: self.close_val}
-# 
-#         cmd_sig = cmd_map[val]
-#         target_val = target_map[val]
-# 
-#         st = self._set_st = DeviceStatus(self)
-#         enums = self.status.enum_strs
-# 
-#         def shutter_cb(value, timestamp, **kwargs):
-#             value = enums[int(value)]
-#             if value == target_val:
-#                 self._set_st._finished()
-#                 self._set_st = None
-#                 self.status.clear_sub(shutter_cb)
-#         uid = str(uuid.uuid4())
-#         cmd_enums = cmd_sig.enum_strs
-#         count = 0
-# 
-#         def cmd_retry_cb(value, timestamp, **kwargs):
-#             nonlocal count
-#             value = cmd_enums[int(value)]
-#             # ts = datetime.datetime.fromtimestamp(timestamp).strftime(_time_fmtstr)
-#             # print('sh', ts, val, st)
-#             if count > 5:
-#                 cmd_sig.clear_sub(cmd_retry_cb)
-#                 st._finished(success=False)
-#             if value == 'None':
-#                 if not st.done:
-#                     import time
-#                     time.sleep(1)
-#                     count += 1
-#                     cmd_sig.set(1)
-#                 else:
-#                     cmd_sig.clear_sub(cmd_retry_cb)
-# 
-#         cmd_sig.subscribe(cmd_retry_cb, run=False)
-#         cmd_sig.set(1)
-#         self.status.subscribe(shutter_cb)
-#         return st
-# 
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         self._set_st = None
-#         self.read_attrs = ['status']
-
-
-shut_fe = TwoButtonShutter('XF:05ID-PPS{Sh:WB}', name='shut_fe')
-shut_a = TwoButtonShutter('XF:05IDA-PPS:1{PSh:2}', name='shut_a')
-shut_b = TwoButtonShutter('XF:05IDB-PPS:1{PSh:4}', name='shut_b')
---
+++ startup/11-optics.py
@@ -0,0 +1,171 @@
+from ophyd import EpicsMotor, EpicsSignal, EpicsSignalRO
+from ophyd import Device
+from ophyd import Component as Cpt
+from ophyd import PVPositionerPC
+from numpy import int16
+from nslsii.devices import TwoButtonShutter
+
+
+### Setup photon shutters
+shut_fe = TwoButtonShutter('XF:05ID-PPS{Sh:WB}', name='shut_fe')
+shut_a = TwoButtonShutter('XF:05IDA-PPS:1{PSh:2}', name='shut_a')
+shut_b = TwoButtonShutter('XF:05IDB-PPS:1{PSh:4}', name='shut_b')
+
+
+### Setup white/pink beam slits
+class SRXSlitsWB(Device):
+    # Real synthetic axes
+    h_cen = Cpt(EpicsMotor, 'XCtr}Mtr')
+    h_gap = Cpt(EpicsMotor, 'XGap}Mtr')
+    v_cen = Cpt(EpicsMotor, 'YCtr}Mtr')
+    v_gap = Cpt(EpicsMotor, 'YGap}Mtr')
+
+    # Real motors
+    top = Cpt(EpicsMotor, 'T}Mtr')
+    bot = Cpt(EpicsMotor, 'B}Mtr')
+    inb = Cpt(EpicsMotor, 'I}Mtr')
+    out = Cpt(EpicsMotor, 'O}Mtr')
+
+class SRXSlitsPB(Device):
+    # Real synthetic axes
+    h_cen = Cpt(EpicsMotor, 'XCtr}Mtr')
+    h_gap = Cpt(EpicsMotor, 'XGap}Mtr')
+
+    # Real motors
+    inb = Cpt(EpicsMotor, 'I}Mtr')
+    out = Cpt(EpicsMotor, 'O}Mtr')
+
+slt_wb = SRXSlitsWB('XF:05IDA-OP:1{Slt:1-Ax:', name='slt_wb')
+slt_pb = SRXSlitsPB('XF:05IDA-OP:1{Slt:2-Ax:', name='slt_pb')
+
+# Is this used?
+class SRXSlits2(Device):
+    inb = Cpt(EpicsMotor, 'I}Mtr')
+    out = Cpt(EpicsMotor, 'O}Mtr')
+
+
+### Setup HFM Mirror
+class SRXHFM(Device):
+    x = Cpt(EpicsMotor, 'X}Mtr')
+    y = Cpt(EpicsMotor, 'Y}Mtr')
+    pitch = Cpt(EpicsMotor, 'P}Mtr')
+    bend = Cpt(EpicsMotor, 'Bend}Mtr')
+
+hfm = SRXHFM('XF:05IDA-OP:1{Mir:1-Ax:', name='hfm')
+
+
+### Setup HDCM
+class HDCMPIEZOROLL(PVPositionerPC):
+    setpoint = Cpt(EpicsSignal, '')
+    readback = Cpt(EpicsSignalRO, '')
+    pid_enabled = EpicsSignal('XF:05IDD-CT{FbPid:01}PID:on')
+    pid_I = EpicsSignal('XF:05IDD-CT{FbPid:01}PID.I')
+
+    def reset_pid(self):
+        yield from bps.mov(self.pid_I, 0.0)
+
+class HDCMPIEZOPITCH(PVPositionerPC):
+    setpoint = Cpt(EpicsSignal, '')
+    readback = Cpt(EpicsSignalRO, '')
+    pid_enabled = EpicsSignal('XF:05IDD-CT{FbPid:02}PID:on')
+    pid_I = EpicsSignal('XF:05IDD-CT{FbPid:02}PID.I')
+
+    def reset_pid(self):
+        yield from bps.mov(self.pid_I, 0.0)
+
+class SRXDCM(Device):
+    bragg = Cpt(EpicsMotor, 'P}Mtr')
+    c1_roll = Cpt(EpicsMotor, 'R1}Mtr')
+    c1_fine = HDCMPIEZOROLL('XF:05ID-BI{EM:BPM1}DAC0', name='c1_roll')
+    c2_x = Cpt(EpicsMotor, 'X2}Mtr')
+    c2_pitch = Cpt(EpicsMotor, 'P2}Mtr')
+    c2_fine = HDCMPIEZOPITCH('XF:05ID-BI{EM:BPM1}DAC1', name='c2_fine')
+    c2_pitch_kill = Cpt(EpicsSignal, 'P2}Cmd:Kill-Cmd')
+    x = Cpt(EpicsMotor, 'X}Mtr')
+    y = Cpt(EpicsMotor, 'Y}Mtr')
+
+    temp_pitch = Cpt(EpicsSignalRO, 'P}T-I')
+
+dcm = SRXDCM('XF:05IDA-OP:1{Mono:HDCM-Ax:' , name='dcm')
+
+
+### Setup BPM motors
+class SRXBPM(Device):
+    y = Cpt(EpicsMotor, 'YFoil}Mtr')
+    diode_x = Cpt(EpicsMotor, 'XDiode}Mtr')
+    diode_y = Cpt(EpicsMotor, 'YDiode}Mtr')
+
+bpm1_pos = SRXBPM('XF:05IDA-BI:1{BPM:1-Ax:', name='bpm1_pos')
+bpm2_pos = SRXBPM('XF:05IDB-BI:1{BPM:2-Ax:', name='bpm2_pos')
+
+
+### Setup SSA
+class SRXSSAHG(PVPositionerPC):
+    setpoint = Cpt(EpicsSignal, 'X}size')
+    readback = Cpt(EpicsSignalRO, 'X}t2.C')
+
+class SRXSSAHC(PVPositionerPC):
+    setpoint = Cpt(EpicsSignal, 'X}center')
+    readback = Cpt(EpicsSignalRO, 'X}t2.D')
+
+class SRXSSAVG(PVPositionerPC):
+    setpoint = Cpt(EpicsSignal, 'Y}size')
+    readback = Cpt(EpicsSignalRO, 'Y}t2.C')
+
+class SRXSSAVC(PVPositionerPC):
+    setpoint = Cpt(EpicsSignal, 'Y}center')
+    readback = Cpt(EpicsSignalRO, 'Y}t2.D')
+
+class SRXSSACalc(Device):
+    h_cen = SRXSSAHC('XF:05IDB-OP:1{Slt:SSA-Ax:')
+    h_gap = SRXSSAHG('XF:05IDB-OP:1{Slt:SSA-Ax:')
+    v_cen = SRXSSAVC('XF:05IDB-OP:1{Slt:SSA-Ax:')
+    v_gap = SRXSSAVG('XF:05IDB-OP:1{Slt:SSA-Ax:')
+
+slt_ssa = SRXSSACalc('XF:05IDB-OP:1{Slt:SSA-Ax:',name='slt_ssa')
+
+
+### Setup fast shutter
+# This is not currently installed at SRX and is commented out
+# class SRXSOFTINP(Device):
+#     pulse = Cpt(EpicsSignal,'')
+#     #these need to be put complete!!
+#     def high_cmd(self):
+#         self.pulse.put(1)
+#     def low_cmd(self):
+#         self.pulse.put(0)
+#     def toggle_cmd(self):
+#         if self.pulse.get() == 0:
+#             self.pulse.put(1)
+#         else:
+#             self.pulse.put(0)
+# shut_fast = SRXSOFTINP('XF:05IDD-ES:1{Sclr:1}UserLED',name='shut_fast')
+# 
+# class SRXFASTSHUT(SRXSOFTINP):
+#     pulse = Cpt(EpicsSignal,':SOFT_IN:B1')
+#     iobit = Cpt(EpicsSignalRO,':SYS_STAT1LO')
+#     def status(self):
+#         self.low_cmd()
+#         shutopen = bool(int16(self.iobit.get()) & int16(2))
+#         if shutopen is True:
+#             return 'Open'
+#         else:
+#             return 'Closed'
+#     def high_cmd(self):
+#         self.pulse.put(1)
+#     def low_cmd(self):
+#         self.pulse.put(0)
+#     def open_cmd(self):
+#         print(self.status())
+#         if self.status() is 'Closed':
+#             print(self.status())
+#         #    self.low_cmd()
+#             self.high_cmd()
+#     def close_cmd(self):
+#         print(self.status())
+#         if self.status() is 'Open':
+#             print(self.status())
+#          #   self.low_cmd()
+#             self.high_cmd()
+# 
+# #shut_fast = SRXFASTSHUT('XF:05IDD-ES:1{Dev:Zebra1}',name='shut_fast')
