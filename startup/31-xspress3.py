cfd631fa11e62ec894797a21bf247a2c93437f86
Andy Kiss <akiss@bnl.gov>
2019-12-09 13:15:45 -0500

    Put xs object initiation in a try-except so one device does not stop loading of all bluesky scripts

--- startup/31-xspress3.py
+++ startup/31-xspress3.py
@@ -214,41 +214,52 @@ class SrxXspress3Detector(SRXXspressTrigger, Xspress3Detector):
             self._mode = SRXMode.step
         return ret
 
-xs = SrxXspress3Detector('XF:05IDD-ES{Xsp:1}:', name='xs')
-xs.channel1.rois.read_attrs = ['roi{:02}'.format(j) for j in [1, 2, 3, 4]]
-xs.channel2.rois.read_attrs = ['roi{:02}'.format(j) for j in [1, 2, 3, 4]]
-xs.channel3.rois.read_attrs = ['roi{:02}'.format(j) for j in [1, 2, 3, 4]]
-xs.hdf5.num_extra_dims.put(0)
-xs.channel2.vis_enabled.put(1)
-xs.channel3.vis_enabled.put(1)
-xs.settings.num_channels.put(3)
-
-xs.settings.configuration_attrs = ['acquire_period',
-			'acquire_time',
-			'gain',
-			'image_mode',
-			'manufacturer',
-			'model',
-			'num_exposures',
-			'num_images',
-			'temperature',
-			'temperature_actual',
-			'trigger_mode',
-			'config_path',
-			'config_save_path',
-			'invert_f0',
-			'invert_veto',
-			'xsp_name',
-			'num_channels',
-			'num_frames_config',
-			'run_flags',
-			'trigger_signal']
-
-
-# This is necessary for when the ioc restarts
-# we have to trigger one image for the hdf5 plugin to work correclty
-# else, we get file writing errors
-xs.hdf5.warmup()
+try:
+    xs = SrxXspress3Detector('XF:05IDD-ES{Xsp:1}:', name='xs')
+    xs.channel1.rois.read_attrs = ['roi{:02}'.format(j) for j in [1, 2, 3, 4]]
+    xs.channel2.rois.read_attrs = ['roi{:02}'.format(j) for j in [1, 2, 3, 4]]
+    xs.channel3.rois.read_attrs = ['roi{:02}'.format(j) for j in [1, 2, 3, 4]]
+    xs.hdf5.num_extra_dims.put(0)
+    xs.channel2.vis_enabled.put(1)
+    xs.channel3.vis_enabled.put(1)
+    xs.settings.num_channels.put(3)
+    
+    xs.settings.configuration_attrs = ['acquire_period',
+    			                       'acquire_time',
+    			                       'gain',
+    			                       'image_mode',
+    			                       'manufacturer',
+    			                       'model',
+    			                       'num_exposures',
+    			                       'num_images',
+    			                       'temperature',
+    			                       'temperature_actual',
+    			                       'trigger_mode',
+    			                       'config_path',
+    			                       'config_save_path',
+    			                       'invert_f0',
+    			                       'invert_veto',
+    			                       'xsp_name',
+    			                       'num_channels',
+    			                       'num_frames_config',
+    			                       'run_flags',
+    			                       'trigger_signal']
+    
+    # This is necessary for when the ioc restarts
+    # we have to trigger one image for the hdf5 plugin to work correclty
+    # else, we get file writing errors
+    xs.hdf5.warmup()
+    
+    # Rename the ROIs
+    for i in range(1,4):
+        ch=getattr(xs.channel1.rois,'roi{:02}.value'.format(i))
+        ch.name = 'ROI_{:02}'.format(i)
+except TimeoutError:
+    xs = None
+    print('\nCannot connect to xs. Continuing without device.\n')
+except:
+    xs = None
+    print('\nUnexpected error connecting to xs.\n', sys.exc_info()[0], end='\n\n')
 
 
 # Working xs2 detector
@@ -332,10 +343,4 @@ except TimeoutError:
 except:
     xs2 = None
     print('\nUnexpected error connecting to xs2.\n', sys.exc_info()[0], end='\n\n')
-
-
-
-for i in range(1,4):
-    ch=getattr(xs.channel1.rois,'roi{:02}.value'.format(i))
-    ch.name = 'ROI_{:02}'.format(i)
 
