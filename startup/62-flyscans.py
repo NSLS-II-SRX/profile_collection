f054c0faa78b941c4c42d80a1e3f6df59eb6b91d
Andy Kiss <akiss@bnl.gov>
2020-02-19 13:47:41 -0500

    Changed acquire_period for dexela

--- startup/62-flyscans.py
+++ startup/62-flyscans.py
@@ -153,7 +153,7 @@ def scan_and_fly_base(detectors, xstart, xstop, xnum, ystart, ystop, ynum, dwell
     if ('dexela' in dets_by_name):
         xrd = dets_by_name['dexela']
         xrd.cam.stage_sigs['acquire_time'] = 0.50 * dwell - 0.050
-        xrd.cam.stage_sigs['acquire_period'] = 1.00 * dwell - 0.020
+        xrd.cam.stage_sigs['acquire_period'] = 0.50 * dwell - 0.020
         del xrd
 
     # If delta is None, set delta based on time for acceleration
