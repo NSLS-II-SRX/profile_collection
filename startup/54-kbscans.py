Tried to fix HFVLM so it could work as a detector Updated sample positions Changed ROI for knife_edge scan from copper to gold Removed normalization with I0 during knife_edge scan Added addition move to initial energy position before xanes scan

startup/54-kbscans.py
@@ -82,8 +82,8 @@ def knife_edge(motor, start, stop, stepsize, acqtime,
     if (use_trans == True):
         y = tbl['it'].values[0] / tbl['im'].values[0]
     else:
-        y = np.sum(np.array(tbl['fluor'])[0][:, :, 794:814], axis=(1, 2))
-        y = y / np.array(tbl['i0'])[0]
+        y = np.sum(np.array(tbl['fluor'])[0][:, :, 961:981], axis=(1, 2))
+        #y = y / np.array(tbl['i0'])[0]
     x = np.array(tbl[pos])[0]
     x = x.astype(np.float64)
     y = y.astype(np.float64)