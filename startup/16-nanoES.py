159ebd54458259508f4d056668d628d3945a7163
Andy Kiss <akiss@bnl.gov>
2020-03-23 09:35:14 -0400

    Added EpicsSignalRO and PVPositionerPC to imports Changed classes to CamelCase Changed nanoKB.*_pitch to components

--- startup/16-nanoES.py
+++ startup/16-nanoES.py
@@ -1,28 +1,29 @@
 print(f'Loading {__file__}...')
 
 
-from ophyd import Device, EpicsMotor, EpicsSignal
+from ophyd import Device, EpicsMotor, EpicsSignal, EpicsSignalRO, PVPositionerPC
 from ophyd import Component as Cpt
 
 
+
 ### nano-KB mirrors
-class SRXNANOKBFINE(PVPositionerPC):
+class SRXNanoKBFine(PVPositionerPC):
     setpoint = Cpt(EpicsSignal, 'SPOS') # XF:05IDD-ES:1{Mir:nKBv-Ax:PC}SPOS
     readback = Cpt(EpicsSignalRO, 'RPOS') # XF:05IDD-ES:1{Mir:nKBh-Ax:PC}RPOS
 
-class SRXNANOKB(Device):
+class SRXNanoKB(Device):
     v_y = Cpt(EpicsMotor, 'vert-Ax:Y}Mtr') # XF:05IDD-ES:1{nKB:vert-Ax:Y}Mtr.RBV
-    v_pitch = SRXNANOKBFINE('XF:05IDD-ES:1{nKB:vert-Ax:PC}', name='nanoKB_v_pitch') # XF:05IDD-ES:1{nKB:vert-Ax:PC}RPOS
+    v_pitch = Cpt(SRXNanoKBFine, 'XF:05IDD-ES:1{nKB:vert-Ax:PC}', name='nanoKB_v_pitch', add_prefix=()) # XF:05IDD-ES:1{nKB:vert-Ax:PC}RPOS
     v_pitch_um = Cpt(EpicsMotor, 'horz-Ax:PC}Mtr') # XF:05IDD-ES:1{nKB:horz-Ax:PC}Mtr.RBV
     h_x = Cpt(EpicsMotor, 'horz-Ax:X}Mtr') # XF:05IDD-ES:1{nKB:horz-Ax:X}Mtr.RBV
-    h_pitch = SRXNANOKBFINE('XF:05IDD-ES:1{nKB:horz-Ax:PC}', name='nanoKB_h_pitch') # XF:05IDD-ES:1{nKB:horz-Ax:PC}RPOS
+    h_pitch = Cpt(SRXNanoKBFine, 'XF:05IDD-ES:1{nKB:horz-Ax:PC}', name='nanoKB_h_pitch', add_prefix=()) # XF:05IDD-ES:1{nKB:horz-Ax:PC}RPOS
     h_pitch_um = Cpt(EpicsMotor, 'vert-Ax:PC}Mtr') # XF:05IDD-ES:1{nKB:vert-Ax:PC}Mtr.RBV
 
-nanoKB = SRXNANOKB('XF:05IDD-ES:1{nKB:', name='nanoKB')
+nanoKB = SRXNanoKB('XF:05IDD-ES:1{nKB:', name='nanoKB')
 
 
 ### High flux sample stages
-class SRXNANOSTAGE(Device):
+class SRXNanoStage(Device):
     x = Cpt(EpicsMotor, 'sx}Mtr') # XF:05IDD-ES:1{nKB:Smpl-Ax:sx}Mtr.RBV
     y = Cpt(EpicsMotor, 'sy}Mtr') # XF:05IDD-ES:1{nKB:Smpl-Ax:sy}Mtr.RBV
     z = Cpt(EpicsMotor, 'sz}Mtr') # XF:05IDD-ES:1{nKB:Smpl-Ax:sz}Mtr.RBV
@@ -33,23 +34,23 @@ class SRXNANOSTAGE(Device):
     topx = Cpt(EpicsMotor, 'xth}Mtr') # XF:05IDD-ES:1{nKB:Smpl-Ax:xth}Mtr.RBV
     topz = Cpt(EpicsMotor, 'zth}Mtr') # XF:05IDD-ES:1{nKB:Smpl-Ax:zth}Mtr.RBV
 
-nano_stage = SRXNANOSTAGE('XF:05IDD-ES:1{nKB:Smpl-Ax:', name='nano_stage')
+nano_stage = SRXNanoStage('XF:05IDD-ES:1{nKB:Smpl-Ax:', name='nano_stage')
 
 
 ### SDD motion
-class SRXNANODET(Device):
+class SRXNanoDet(Device):
     x = Cpt(EpicsMotor, 'X}Mtr') # XF:05IDD-ES:1{nKB:Det-Ax:X}Mtr.RBV
     y = Cpt(EpicsMotor, 'Y}Mtr') # XF:05IDD-ES:1{nKB:Det-Ax:Y}Mtr.RBV
     z = Cpt(EpicsMotor, 'Z}Mtr') # XF:05IDD-ES:1{nKB:Det-Ax:Z}Mtr.RBV
 
-nano_det = SRXNANODET('XF:05IDD-ES:1{nKB:Det-Ax:', name='nano_det')
+nano_det = SRXNanoDet('XF:05IDD-ES:1{nKB:Det-Ax:', name='nano_det')
 
 
 # Lakeshore temperature monitors
-class SRXNANOTEMP(Device):
+class SRXNanoTemp(Device):
     temp_nanoKB_horz = Cpt(EpicsSignalRO, '2}T:C-I')
     temp_nanoKB_vert = Cpt(EpicsSignalRO, '1}T:C-I')
     temp_nanoKB_base = Cpt(EpicsSignalRO, '4}T:C-I')
 
-temp_nanoKB = SRXNANOTEMP('XF:05IDD-ES{LS:1-Chan:', name='temp_nanoKB')
+temp_nanoKB = SRXNanoTemp('XF:05IDD-ES{LS:1-Chan:', name='temp_nanoKB')
 
