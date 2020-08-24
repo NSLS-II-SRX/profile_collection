e8f37304b5dd0be1b2b086c3a7f78bae1fd4678e
Maksim Rakitin <mrakitin@bnl.gov>
2020-08-24 14:47:29 -0400

    Add PersistentDict definition

--- startup/00-base.py
+++ startup/00-base.py
@@ -41,3 +41,50 @@ bec.disable_plots()
 # import logging
 # ophyd.logger.setLevel(logging.DEBUG)
 # logging.basicConfig(level=logging.DEBUG)
+
+from pathlib import Path
+
+import appdirs
+
+
+try:
+    from bluesky.utils import PersistentDict
+except ImportError:
+    import msgpack
+    import msgpack_numpy
+    import zict
+
+    class PersistentDict(zict.Func):
+        def __init__(self, directory):
+            self._directory = directory
+            self._file = zict.File(directory)
+            super().__init__(self._dump, self._load, self._file)
+
+        @property
+        def directory(self):
+            return self._directory
+
+        def __repr__(self):
+            return f"<{self.__class__.__name__} {dict(self)!r}>"
+
+        @staticmethod
+        def _dump(obj):
+            "Encode as msgpack using numpy-aware encoder."
+            # See https://github.com/msgpack/msgpack-python#string-and-binary-type
+            # for more on use_bin_type.
+            return msgpack.packb(
+                obj,
+                default=msgpack_numpy.encode,
+                use_bin_type=True)
+
+        @staticmethod
+        def _load(file):
+            return msgpack.unpackb(
+                file,
+                object_hook=msgpack_numpy.decode,
+                raw=False)
+
+runengine_metadata_dir = appdirs.user_data_dir(appname="bluesky") / Path("runengine-metadata")
+
+# PersistentDict will create the directory if it does not exist
+RE.md = PersistentDict(runengine_metadata_dir)
