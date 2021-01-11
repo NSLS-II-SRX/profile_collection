0f383b63f2f8e4fdf893c3be01b67953b4b48fcc
Maksim Rakitin <mrakitin@bnl.gov>
2021-01-11 17:02:37 -0500

    Deployment 2021-1 updates

--- startup/00-base.py
+++ startup/00-base.py
@@ -34,13 +34,6 @@ bec.disable_table()
 
 # Disable BestEffortCallback to plot ring current
 bec.disable_plots()
-
-
-# Temporary fix before it's fixed in ophyd
-import logging
-logger = logging.getLogger('ophyd')
-logger.setLevel('WARNING')
-
 from pathlib import Path
 
 import appdirs
@@ -54,14 +47,48 @@ except ImportError:
     import zict
 
     class PersistentDict(zict.Func):
+        """
+        A MutableMapping which syncs it contents to disk.
+        The contents are stored as msgpack-serialized files, with one file per item
+        in the mapping.
+        Note that when an item is *mutated* it is not immediately synced:
+        >>> d['sample'] = {"color": "red"}  # immediately synced
+        >>> d['sample']['shape'] = 'bar'  # not immediately synced
+        but that the full contents are synced to disk when the PersistentDict
+        instance is garbage collected.
+        """
         def __init__(self, directory):
             self._directory = directory
             self._file = zict.File(directory)
+            self._cache = {}
             super().__init__(self._dump, self._load, self._file)
+            self.reload()
+
+            # Similar to flush() or _do_update(), but without reference to self
+            # to avoid circular reference preventing collection.
+            # NOTE: This still doesn't guarantee call on delete or gc.collect()!
+            #       Explicitly call flush() if immediate write to disk required.
+            def finalize(zfile, cache, dump):
+                zfile.update((k, dump(v)) for k, v in cache.items())
+
+            import weakref
+            self._finalizer = weakref.finalize(
+                self, finalize, self._file, self._cache, PersistentDict._dump)
 
         @property
         def directory(self):
             return self._directory
+
+        def __setitem__(self, key, value):
+            self._cache[key] = value
+            super().__setitem__(key, value)
+
+        def __getitem__(self, key):
+            return self._cache[key]
+
+        def __delitem__(self, key):
+            del self._cache[key]
+            super().__delitem__(key)
 
         def __repr__(self):
             return f"<{self.__class__.__name__} {dict(self)!r}>"
@@ -82,6 +109,16 @@ except ImportError:
                 file,
                 object_hook=msgpack_numpy.decode,
                 raw=False)
+
+        def flush(self):
+            """Force a write of the current state to disk"""
+            for k, v in self.items():
+                super().__setitem__(k, v)
+
+        def reload(self):
+            """Force a reload from disk, overwriting current cache"""
+            self._cache = dict(super().items())
+
 
 # runengine_metadata_dir = appdirs.user_data_dir(appname="bluesky") / Path("runengine-metadata")
 runengine_metadata_dir = Path('/nsls2/xf05id1/shared/config/runengine-metadata')
