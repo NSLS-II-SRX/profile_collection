392d69968adc1bb9a735f51332fb764e1455dc5b
Maksim Rakitin <mrakitin@bnl.gov>
2021-01-14 22:34:29 -0500

    Sync monkey-patches of EpicsSignal*.wait_for_connection with other profiles

--- startup/00-base.py
+++ startup/00-base.py
@@ -1,15 +1,44 @@
 print(f"Loading {__file__}...")
 
-from ophyd.signal import EpicsSignalBase, EpicsSignal
-EpicsSignalBase.set_defaults(connection_timeout=10)
+###############################################################################
+# TODO: remove this block once https://github.com/bluesky/ophyd/pull/959 is
+# merged/released.
+from datetime import datetime
+from ophyd.signal import EpicsSignalBase, EpicsSignal, DEFAULT_CONNECTION_TIMEOUT
 
-def wait_for_connection(self, timeout=0):
+def print_now():
+    return datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S.%f')
+
+def wait_for_connection_base(self, timeout=DEFAULT_CONNECTION_TIMEOUT):
     '''Wait for the underlying signals to initialize or connect'''
-    if timeout == 0:
+    if timeout is DEFAULT_CONNECTION_TIMEOUT:
         timeout = self.connection_timeout
-    self._ensure_connected(self._read_pv, self._write_pv, timeout=timeout)
+    # print(f'{print_now()}: waiting for {self.name} to connect within {timeout:.4f} s...')
+    start = time.time()
+    try:
+        self._ensure_connected(self._read_pv, timeout=timeout)
+        # print(f'{print_now()}: waited for {self.name} to connect for {time.time() - start:.4f} s.')
+    except TimeoutError:
+        if self._destroyed:
+            raise DestroyedError('Signal has been destroyed')
+        raise
 
+def wait_for_connection(self, timeout=DEFAULT_CONNECTION_TIMEOUT):
+    '''Wait for the underlying signals to initialize or connect'''
+    if timeout is DEFAULT_CONNECTION_TIMEOUT:
+        timeout = self.connection_timeout
+    # print(f'{print_now()}: waiting for {self.name} to connect within {timeout:.4f} s...')
+    start = time.time()
+    self._ensure_connected(self._read_pv, self._write_pv, timeout=timeout)
+    # print(f'{print_now()}: waited for {self.name} to connect for {time.time() - start:.4f} s.')
+
+EpicsSignalBase.wait_for_connection = wait_for_connection_base
 EpicsSignal.wait_for_connection = wait_for_connection
+###############################################################################
+
+from ophyd.signal import EpicsSignalBase
+# EpicsSignalBase.set_default_timeout(timeout=10, connection_timeout=10)  # old style
+EpicsSignalBase.set_defaults(timeout=10, connection_timeout=10)  # new style
 
 import nslsii
 import matplotlib as mpl
