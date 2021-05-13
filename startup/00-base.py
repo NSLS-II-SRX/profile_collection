82d58fcd3e937021a3b076e0d125c600a6a0f87b
Andy Kiss <akiss@bnl.gov>
2021-05-13 09:44:37 -0400

    Rebased with master. Changed metadata folder.

--- startup/00-base.py
+++ startup/00-base.py
@@ -164,6 +164,6 @@ except ImportError:
 
 
 # runengine_metadata_dir = appdirs.user_data_dir(appname="bluesky") / Path("runengine-metadata")
-runengine_metadata_dir = Path('/nsls2/xf05id1/shared/config/runengine-metadata')
+runengine_metadata_dir = Path('/nsls2/xf05id1/shared/config/runengine-metadata-new')
 
 RE.md = PersistentDict(runengine_metadata_dir)
