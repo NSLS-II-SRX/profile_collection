67daedd9bcc126fd7976d45237a4f9425f23a79d
Andy Kiss <akiss@bnl.gov>
2021-11-29 11:38:13 -0500

    Print the scan ID at the beginning of the scan

--- startup/45-scanrecord-cb.py
+++ startup/45-scanrecord-cb.py
@@ -7,4 +7,12 @@ def update_scanbroker(name, doc):
     elif name == "stop":
         scanrecord.scanning.put(False)
 
+def force_print_scanid(name, doc):
+    if name == "start":
+        print(f"Scan ID: {doc['scan_id']}")
+    # elif name == "stop":
+    #     print(f"Scan ID: {doc['scan_id']}")
+
+
 RE.subscribe(update_scanbroker)
+RE.subscribe(force_print_scanid)
