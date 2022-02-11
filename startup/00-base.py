9301accaa2e97392a603b9420af656df15292083
Andy Kiss <akiss@bnl.gov>
2022-02-11 15:35:49 -0500

    Moved PersistantDict to Lustre

--- startup/00-base.py
+++ startup/00-base.py
@@ -160,7 +160,8 @@ except ImportError:
 
 
 # runengine_metadata_dir = appdirs.user_data_dir(appname="bluesky") / Path("runengine-metadata")
-runengine_metadata_dir = Path('/nsls2/xf05id1/shared/config/runengine-metadata-new')
+# runengine_metadata_dir = Path('/nsls2/xf05id1/shared/config/runengine-metadata-new')
+runengine_metadata_dir = Path('/nsls2/data/srx/legacy/xf05id1/shared/config/runengine-metadata-new')
 
 RE.md = PersistentDict(runengine_metadata_dir)
 
