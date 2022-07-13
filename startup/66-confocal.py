4f1a199759c05d680115382e79932a7ebbe17b1a
Andy Kiss <akiss@bnl.gov>
2022-07-13 11:48:21 -0400

    Changes after confocal experiments
    - Added confocal slits into profile
    - Modified confocal-specific plans to use xs instead of xs2

--- startup/66-confocal.py
+++ startup/66-confocal.py
@@ -4,19 +4,19 @@
         "dets": {"default": "['xs2', 'sclr1']"},
     }
 })
-def xs2_1d_scan(stage, x0, x1, nx, dwell, dets=[xs2, sclr1], shutter=True):
+def xs2_1d_scan(stage, x0, x1, nx, dwell, dets=[xs, sclr1], shutter=True):
     yield from check_shutters(shutter, 'Open')
-    yield from mov(xs2.external_trig, False)
-    yield from mov(xs2.cam.acquire_time, dwell)
-    yield from mov(xs2.total_points, nx)
-    yield from mov(xs2.cam.num_images, nx)
+    yield from mov(xs.external_trig, False)
+    yield from mov(xs.cam.acquire_time, dwell)
+    yield from mov(xs.total_points, nx)
+    yield from mov(xs.cam.num_images, nx)
     yield from mov(sclr1.preset_time, dwell)
     yield from subs_wrapper(
         scan(dets, stage, x0, x1, nx),
         {
             "all": [
                 LivePlot(
-                    "xs2_channels_channel01_mcarois_mcaroi01_total_rbv",
+                    "xs_channel08_mcaroi01_total_rbv",
                     x=stage.name,
                 )
             ]
@@ -30,19 +30,19 @@ def xs2_1d_scan(stage, x0, x1, nx, dwell, dets=[xs2, sclr1], shutter=True):
         "dets": {"default": "['xs2', 'sclr1']"},
     }
 })
-def xs2_1d_relscan(stage, neg_dx, pos_dx, nx, dwell, dets=[xs2, sclr1], shutter=True):
+def xs2_1d_relscan(stage, neg_dx, pos_dx, nx, dwell, dets=[xs, sclr1], shutter=True):
     yield from check_shutters(shutter, 'Open')
-    yield from mov(xs2.external_trig, False)
-    yield from mov(xs2.cam.acquire_time, dwell)
-    yield from mov(xs2.total_points, nx)
-    yield from mov(xs2.cam.num_images, nx)
+    yield from mov(xs.external_trig, False)
+    yield from mov(xs.cam.acquire_time, dwell)
+    yield from mov(xs.total_points, nx)
+    yield from mov(xs.cam.num_images, nx)
     yield from mov(sclr1.preset_time, dwell)
     yield from subs_wrapper(
         rel_scan(dets, stage, neg_dx, pos_dx, nx),
         {
             "all": [
                 LivePlot(
-                    "xs2_channels_channel01_mcarois_mcaroi01_total_rbv",
+                    "xs_channel08_mcaroi01_total_rbv",
                     x=stage.name,
                 )
             ]
@@ -51,12 +51,12 @@ def xs2_1d_relscan(stage, neg_dx, pos_dx, nx, dwell, dets=[xs2, sclr1], shutter=
     yield from check_shutters(shutter, 'Close')
 
 def overnight():
-    x0 = 4.2
-    x1 = 4.2
-    nx = 1
+    x0 = -2.0
+    x1 = 2.0
+    nx = 41
 
     x = np.linspace(x0, x1, num=nx)
     for xi in x:
         yield from mov(confocal_stage.x, xi)
-        yield from xs2_1d_relscan(confocal_stage.y, -0.5, 0.5, 101, 1)
-        yield from xs2_1d_relscan(confocal_stage.z, -0.5, 0.5, 101, 1)
+        yield from xs2_1d_relscan(confocal_stage.y, -0.1, 0.1, 41, .5)
+        yield from xs2_1d_relscan(confocal_stage.z, -0.1, 0.1, 41, .5)
