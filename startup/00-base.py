fb9538909862264eabcb1f3c3efe5b7d54b8692a
Max Rakitin <mrakitin@bnl.gov>
2024-02-16 19:05:04 -0500

    STY: Apply `black` and `isort`
    (2024-1.0-py310-tiled) [xf05id1@xf05id2-ws7 startup]$ # isort -l 120 00-base.py; black -l 120 00-base.py
    (2024-1.0-py310-tiled) [xf05id1@xf05id2-ws7 startup]$ # isort -l 120 02-usersetup.py; black -l 120 02-usersetup.py

--- startup/00-base.py
+++ startup/00-base.py
@@ -1,19 +1,19 @@
 print(f"Loading {__file__}...")
 
-import os
 import copy
-import redis
-from redis_json_dict import RedisJSONDict
-from datetime import datetime
-from ophyd.signal import EpicsSignalBase, EpicsSignal, DEFAULT_CONNECTION_TIMEOUT
-from bluesky_queueserver import is_re_worker_active, parameter_annotation_decorator
-from tiled.client import from_profile
-from ophyd.signal import EpicsSignalBase
 import datetime
-import nslsii
-import matplotlib as mpl
-from IPython.terminal.prompts import Prompts, Token
+import os
+from datetime import datetime
+from pathlib import Path
 
+import matplotlib as mpl
+import nslsii
+import redis
+from bluesky_queueserver import is_re_worker_active, parameter_annotation_decorator
+from IPython.terminal.prompts import Prompts, Token
+from ophyd.signal import DEFAULT_CONNECTION_TIMEOUT, EpicsSignal, EpicsSignalBase
+from redis_json_dict import RedisJSONDict
+from tiled.client import from_profile
 
 
 def if_touch_beamline(envvar="TOUCHBEAMLINE"):
@@ -25,11 +25,13 @@ def if_touch_beamline(envvar="TOUCHBEAMLINE"):
     else:
         raise ValueError(f"Unknown value: {value}")
 
+
 def print_now():
-    return datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S.%f')
+    return datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S.%f")
+
 
 def wait_for_connection_base(self, timeout=DEFAULT_CONNECTION_TIMEOUT):
-    '''Wait for the underlying signals to initialize or connect'''
+    """Wait for the underlying signals to initialize or connect"""
     if timeout is DEFAULT_CONNECTION_TIMEOUT:
         timeout = self.connection_timeout
     # print(f'{print_now()}: waiting for {self.name} to connect within {timeout:.4f} s...')
@@ -39,17 +41,19 @@ def wait_for_connection_base(self, timeout=DEFAULT_CONNECTION_TIMEOUT):
         # print(f'{print_now()}: waited for {self.name} to connect for {time.time() - start:.4f} s.')
     except TimeoutError:
         if self._destroyed:
-            raise DestroyedError('Signal has been destroyed')
+            raise DestroyedError("Signal has been destroyed")
         raise
 
+
 def wait_for_connection(self, timeout=DEFAULT_CONNECTION_TIMEOUT):
-    '''Wait for the underlying signals to initialize or connect'''
+    """Wait for the underlying signals to initialize or connect"""
     if timeout is DEFAULT_CONNECTION_TIMEOUT:
         timeout = self.connection_timeout
     # print(f'{print_now()}: waiting for {self.name} to connect within {timeout:.4f} s...')
     start = time.time()
     self._ensure_connected(self._read_pv, self._write_pv, timeout=timeout)
     # print(f'{print_now()}: waited for {self.name} to connect for {time.time() - start:.4f} s.')
+
 
 EpicsSignalBase.wait_for_connection = wait_for_connection_base
 EpicsSignal.wait_for_connection = wait_for_connection
@@ -64,16 +68,14 @@ else:
     timeout = 10  # seconds
     going = "NOT going"
 
-print(f'\nEpicsSignalBase timeout is {timeout} [seconds]. {going} to touch beamline hardware.\n')
+print(f"\nEpicsSignalBase timeout is {timeout} [seconds]. {going} to touch beamline hardware.\n")
 
 # EpicsSignalBase.set_default_timeout(timeout=timeout, connection_timeout=timeout)  # old style
 EpicsSignalBase.set_defaults(timeout=timeout, connection_timeout=timeout)  # new style
 
 
-
 def print_now():
-    return datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S.%f')
-
+    return datetime.datetime.strftime(datetime.datetime.now(), "%Y-%m-%d %H:%M:%S.%f")
 
 
 ip = get_ipython()
@@ -89,7 +91,7 @@ RE.unsubscribe(0)
 
 # Define tiled catalog
 c = from_profile("srx")
-srx_raw = from_profile('nsls2', api_key=os.environ["TILED_BLUESKY_WRITING_API_KEY_SRX"])['srx']['raw']
+srx_raw = from_profile("nsls2", api_key=os.environ["TILED_BLUESKY_WRITING_API_KEY_SRX"])["srx"]["raw"]
 
 
 def post_document(name, doc):
@@ -97,9 +99,10 @@ def post_document(name, doc):
         doc = copy.deepcopy(doc)
     srx_raw.post_document(name, doc)
 
+
 RE.subscribe(post_document)
 
-ip.log.setLevel('WARNING')
+ip.log.setLevel("WARNING")
 
 nslsii.configure_olog(ip.user_ns)
 
@@ -114,27 +117,31 @@ bec.disable_table()
 # Disable BestEffortCallback to plot ring current
 bec.disable_plots()
 
-from pathlib import Path
-
 RE.md = RedisJSONDict(redis.Redis("info.srx.nsls2.bnl.gov"), prefix="bsui")
 
 # Optional: set any metadata that rarely changes.
 RE.md["beamline_id"] = "SRX"
 RE.md["md_version"] = "1.1"
 
+
 class SRXPrompt(Prompts):
     def in_prompt_tokens(self, cli=None):
         return [
-            (Token.Prompt, f"☁️  BlueSky@SRX | Proposal #{RE.md.get('proposal', {}).get('proposal_num', 'N/A')} ["),
+            (
+                Token.Prompt,
+                f"☁️  BlueSky@SRX | Proposal #{RE.md.get('proposal', {}).get('proposal_num', 'N/A')} [",
+            ),
             (Token.PromptNum, str(self.shell.execution_count)),
             (Token.Prompt, "]: "),
         ]
+
+
 ip.prompts = SRXPrompt(ip)
 ""
 # from bluesky.utils import ts_msg_hook
 # RE.msg_hook = ts_msg_hook
 
-# The following plan stubs are automatically imported in global namespace by 'nslsii.configure_base', 
+# The following plan stubs are automatically imported in global namespace by 'nslsii.configure_base',
 # but have signatures that are not compatible with the Queue Server. They should not exist in the global
 # namespace, but can be accessed as 'bps.one_1d_step' etc. from other plans.
 del one_1d_step, one_nd_step, one_shot
--- startup/02-usersetup.py
+++ startup/02-usersetup.py
@@ -1,8 +1,9 @@
-print(f'Loading {__file__}...')
+print(f"Loading {__file__}...")
 
 import os
-import time as ttime
 import shutil
+import time as ttime
+
 import httpx
 
 ### Proposal information put into the metadata
@@ -19,13 +20,9 @@ proposal_num = data_session.split("-")[-1]
 # PI_lastname = 'Kiss'
 # saf_num = 311284
 
-nslsii_api_client = httpx.Client(
-    base_url="https://api-staging.nsls2.bnl.gov"
-)
+nslsii_api_client = httpx.Client(base_url="https://api-staging.nsls2.bnl.gov")
 
-proposal_response = nslsii_api_client.get(
-    f"/proposal/{proposal_num}"
-)
+proposal_response = nslsii_api_client.get(f"/proposal/{proposal_num}")
 proposal_response.raise_for_status()
 proposal = proposal_response.json()
 
@@ -38,11 +35,11 @@ for user in proposal["users"]:
 saf_num = proposal["safs"][0]["saf_id"]
 
 md_proposal = {
-    'proposal_num' :  proposal_num,
-    'proposal_title' : proposal["title"],
-    'PI_lastname' : PI_lastname,
-    'saf_num' : saf_num,
-    'cycle' : proposal["cycles"][-1],
+    "proposal_num": proposal_num,
+    "proposal_title": proposal["title"],
+    "PI_lastname": PI_lastname,
+    "saf_num": saf_num,
+    "cycle": proposal["cycles"][-1],
 }
 
 # proposal_num = 313507
@@ -51,8 +48,7 @@ md_proposal = {
 # saf_num = 312779
 
 
-
-cycle = '2024_cycle1'
+cycle = "2024_cycle1"
 
 # Set user data in bluesky
 # RE.md['data_session'] = data_session
